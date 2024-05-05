a2d6ebf895be546a1a16083f485abcd8c352fe26
Sean Clifford <seancliff01@gmail.com>
2024-05-05 12:59:59 -0400

    cuda or cpu check

--- track.py
+++ track.py
@@ -1,27 +1,26 @@
 from tracker import Tracker
 import cv2
+import torch
 
 
 def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     tracker = Tracker(
         embedding_model_name='google/vit-base-patch16-224-in21k',
         yolo_model='yolov8m.pt',
         num_tables=10,
         max_add_images=10,
         multiprocessing=False,
-        device='cpu',
+        device=device,
         verbose=True
     )
     # Average time: 0.02482800139594324 Max time: 1.383957862854004
     cap = cv2.VideoCapture(0)
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+        _, frame = cap.read()
 
         results = tracker.track(frame)
-        if results:
-            results.plot()
+        results.plot()
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
