48fbad3350f333be9213074453eb0280ef69d655
Sean Clifford <seancliff01@gmail.com>
2024-05-05 12:43:48 -0400

    added arguments to tracker

--- track.py
+++ track.py
@@ -7,10 +7,12 @@ def main():
         embedding_model_name='google/vit-base-patch16-224-in21k',
         yolo_model='yolov8m.pt',
         num_tables=10,
-        device='cuda',
+        max_add_images=10,
+        multiprocessing=False,
+        device='cpu',
         verbose=True
     )
-
+    # Average time: 0.02482800139594324 Max time: 1.383957862854004
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
